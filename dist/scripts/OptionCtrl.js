c0ecabebaf2ae2f1b1ff19e4a12327fe199199e7
Onur Sahin <anionrush@gmail.com>
2016-07-07 05:12:31 -0500

    Basic structure

---
+++ dist/scripts/OptionCtrl.js
@@ -0,0 +1,12 @@
+(function() {
+    
+    function OptionCtrl(){
+        this.row_titles = ['','Small Tree', 'Medium Tree', 'Large Tree'];
+        this.col_titles = ['Dead/Sucker Removal', 'Medium', 'Full', 'Tree Removal to Stump' ]
+        this.img_arrays = [[],[],[]];
+        
+    }
+    
+    angular.module('treeMendous')
+        .controller('OptionCtrl', OptionCtrl)
+})();
