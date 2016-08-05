eae342a53feef438d4dbe575628bf9df7c3abf4f
Onur Sahin <anionrush@gmail.com>
2016-08-05 14:34:28 -0500

    Firebase added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,38 +1,39 @@
 (function() {
             
-            function config($stateProvider, $locationProvider) {
-                $locationProvider
-                .html5Mode({
-                    enabled: true,
-                    requireBase: false
-                });
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+        .html5Mode({
+            enabled: true,
+            requireBase: false
+        });
 
-                $stateProvider
-                .state('/', {
-                    url: '/',
-                    templateUrl: '/templates/home.html'
-                 })
-                .state('bio', {
-                    url: '/bio',
-                    templateUrl: '/templates/bio.html'
-                 })
-                .state('options', {
-                    url: '/options',
-                    controller: 'OptionCtrl as optn',
-                    templateUrl: '/templates/options.html'
-                })
-                .state('consultation', {
-                    url: '/consultation',
-                    templateUrl: '/templates/consultation.html'
-                })
-                .state('admin', {
-                    url: '/admin',
-                    controller: 'ApptCtrl as appt',
-                    templateUrl: '/templates/admin.html'
-                });
-            }
+        $stateProvider
+        .state('/', {
+            url: '/',
+            templateUrl: '/templates/home.html'
+         })
+        .state('bio', {
+            url: '/bio',
+            templateUrl: '/templates/bio.html'
+         })
+        .state('options', {
+            url: '/options',
+            controller: 'OptionCtrl as optn',
+            templateUrl: '/templates/options.html'
+        })
+        .state('consultation', {
+            url: '/consultation',
+            controller: 'ApptCtrl as appt',
+            templateUrl: '/templates/consultation.html'
+        })
+        .state('admin', {
+            url: '/admin',
+            controller: 'ApptCtrl as appt',
+            templateUrl: '/templates/admin.html'
+        });
+    }
 
             
-            angular.module('treeMendous', ['firebase','ui.router'])
-                .config(config);
-        })();
+    angular.module('treeMendous', ['firebase','ui.router'])
+        .config(config);
+})();
--- dist/scripts/ApptCtrl.js
+++ dist/scripts/ApptCtrl.js
@@ -1,7 +1,26 @@
 (function() {
     
     function ApptCtrl($firebaseArray){
-         
+        var ref = new Firebase('https://treemendous-b5030.firebaseio.com/appointments');
+        this.apptArray = $firebaseArray(ref);
+        this.addAppt = function(){
+            this.apptArray.$add({
+                name: this.nameText,
+                phone: this.phoneText,
+                date: this.dateText,
+                time: this.timeText,
+                address: this.addressText,
+                description: this.descriptionText
+            })
+            
+            this.nameText=null;
+            this.phoneText=null;
+            this.dateText=null;
+            this.timeText=null;
+            this.addressText=null;
+            this.descriptionText=null;
+            
+        };
     }
     
     angular.module('treeMendous')
